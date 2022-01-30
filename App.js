575cd85507bb9b3ee3497fe89d70eee894bb9d55
bonettimrc <bonetti.mrc@gmail.com>
2022-01-30 01:17:40 +0100

    multiplayer

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Game from './components/Game';
-import Menu from './components/Menu';
-const Stack = createNativeStackNavigator();
+import MultiPlayerGame from './components/MultiPlayerGame';
+
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="menu" component={Menu} options={{ title: 'Menu' }}></Stack.Screen>
-        <Stack.Screen name="singlePlayer" component={Game} options={{ title: 'SinglePlayer' }}></Stack.Screen>
-        <Stack.Screen name="multiPlayer" component={Game} options={{ title: 'MultiPlayer' }}></Stack.Screen>
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="singlePlayer" component={Game} options={{
+          title: 'SinglePlayer', tabBarIcon: ({ color, size }) => {
+            return <MaterialCommunityIcons name="grid" color={color} size={size} />
+          }
+        }}></Tab.Screen>
+        <Tab.Screen name="multiPlayer" component={MultiPlayerGame} options={{
+          title: 'MultiPlayer', tabBarIcon: ({ color, size }) => {
+            return <MaterialCommunityIcons name="earth" color={color} size={size} />
+          }
+        }}></Tab.Screen>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- components/MultiPlayerGame.js
+++ components/MultiPlayerGame.js
@@ -1,67 +1,132 @@
-import React from "react";
-import { Button, StyleSheet, Text, View } from 'react-native';
-import Board from "./Board";
+import React from 'react';
+import { ActivityIndicator, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { io } from 'socket.io-client';
+import Board from './Board';
+// fuck hooks 😠
 export default class MultiPlayerGame extends React.Component {
     constructor(props) {
         super(props)
-        this.state = this.getInitialState()
-        this.onTileClicked = this.onTileClicked.bind(this);
+        this.state = {
+            socket: null,
+            roomId: null,
+            tiles: Array(9).fill(null),
+            imX: null,
+            currentTurn: 0,
+            secondPlayerJoined: false
+        }
+        this.onTileClicked = this.onTileClicked.bind(this)
+        this.onRoomEntered = this.onRoomEntered.bind(this)
+        this.onRoomCreated = this.onRoomCreated.bind(this)
     }
-    createRoom(id) {
-        fetch(`bonettimrc.ddns.net:5000/room/${id}`, {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
+    componentDidMount() {
+        const newSocket = io(`http://bonettimrc.ddns.net:5000`);
+        newSocket.connect()
+        newSocket.on("gameMove", ({ i }) => {
+            const tiles = [...this.state.tiles]
+            tiles[i] = !this.state.imX ? 'X' : 'O'
+            this.setState({
+                tiles,
+                currentTurn: this.state.currentTurn + 1
+            })
         })
-            .then(res => res.json())
-            .then(res => { this.setState(res) });
-    }
-    getRoom(id) {
-        fetch(`bonettimrc.ddns.net:5000/room/${id}`, {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
+        newSocket.on("secondPlayerJoined", () => {
+            this.setState({
+                secondPlayerJoined: true
+            })
         })
-            .then(res => res.json())
-            .then(res => { this.setState(res) });
+        this.setState({
+            socket: newSocket
+        })
     }
-    co
+    componentWillUnmount() {
+        this.state.socket.close()
+    }
+    onRoomEntered({ nativeEvent: { text, eventCount, target } }) {
+        this.state.socket.emit("joinRoom", text)
+        this.setState({
+            roomId: text,
+            imX: false
+        })
+        this.setState({ secondPlayerJoined: true })
+    }
+    onRoomCreated({ nativeEvent: { text, eventCount, target } }) {
+        this.state.socket.emit("createRoom", text)
+        this.state.socket.emit("joinRoom", text)
+        this.setState({
+            roomId: text,
+            imX: true
+        })
+    }
     onTileClicked(i) {
-        // TODO
-    }
-    onPlayAgain() {
-        // TODO
+        const currentTurnIsX = (this.state.currentTurn % 2) == 0
+        if (this.state.imX != currentTurnIsX) return
+        const tiles = [...this.state.tiles]
+        if (calculateWinningLine(tiles) || tiles[i]) return
+        tiles[i] = this.state.imX ? 'X' : 'O'
+        this.state.socket.emit("gameMove", { i })
+        this.setState({
+            tiles,
+            currentTurn: this.state.currentTurn + 1
+        })
+
     }
     render() {
-        const winningLine = this.state.winningLine;
-        const currentPlayerIsX = this.state.currentPlayerIsX;
+        const winningLine = calculateWinningLine(this.state.tiles)
+        const currentTurnIsX = (this.state.currentTurn % 2) == 0
         let status;
-        let resetGameText;
         if (winningLine) {
-            status = `The winner is ${currentPlayerIsX ? 'O' : 'X'}`
-            resetGameText = "Play Again"
+            status = `The winner is ${currentTurnIsX ? 'O' : 'X'}`
         }
         else {
             if (this.state.currentTurn < 9) {
-                status = `It's ${currentPlayerIsX ? 'X' : 'O'} turn`
+                status = `It's ${currentTurnIsX ? 'X' : 'O'} turn`
             } else {
                 status = "It's a draw"
             }
-            resetGameText = "Reset Game"
         }
         return (
+
             <View style={styles.container}>
-                <Text style={styles.statusText}>{status}</Text>
-                <Board tiles={this.state.tiles} onTileClicked={this.onTileClicked} winningLine={winningLine}></Board>
-                <Button onPress={() => { this.onPlayAgain() }} title={resetGameText}></Button>
-            </View>);
+                {this.state.roomId ? <>
+                    <Text style={styles.statusText}>{`room:${this.state.roomId}`}</Text>
+                    {this.state.secondPlayerJoined ? <>
+                        <Text style={styles.statusText}>{`you are ${this.state.imX ? "X" : "O"}`}</Text>
+                        <Text style={styles.statusText}>{status}</Text>
+                        <Board tiles={this.state.tiles} onTileClicked={this.onTileClicked} winningLine={winningLine}></Board>
+                        {winningLine ? <>
+                            <Button title='Replay' />
+                            <Button title='Quit' color="#BB2D3B" />
+                        </> : <></>}
+                    </> : <>
+                        <ActivityIndicator size="large" color="#0000ff" />
+                    </>}
+                </> : <>
+                    <TextInput onSubmitEditing={this.onRoomCreated} placeholder='Create Room' style={styles.input} />
+                    <TextInput onSubmitEditing={this.onRoomEntered} placeholder='Enter Room' style={styles.input} /></>}
+            </View>
+
+        );
     }
 
-
+}
+function calculateWinningLine(tiles) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],];
+    for (const line of lines) {
+        const values = line.map(i => tiles[i])
+        const winner = values.reduce((previousValue, currentValue) => previousValue === currentValue ? currentValue : null);
+        if (winner) {
+            return line
+        }
+    }
+    return null;
 }
 const styles = StyleSheet.create({
     container: {
@@ -73,5 +138,11 @@ const styles = StyleSheet.create({
     statusText: {
         fontSize: 30,
         fontWeight: "bold"
-    }
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
 });
